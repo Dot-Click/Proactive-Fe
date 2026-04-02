dc6090193922d75b4949f5ec673ee2bd73574422
Dot Click LLC <129838150+Dot-Click@users.noreply.github.com>
2026-04-02 13:29:17 +0500

    ...

--- src/pages/userSide/home.page.tsx
+++ src/pages/userSide/home.page.tsx
@@ -7,7 +7,8 @@ import Whatwedo from "@/components/userSide/Home/whatwedo/Whatwedo"
 
 const HomePage = () => {
   return (
-    <div className="bg-[#F9FBFE]">
+        <div className="bg-[#F2F6FD]">
+
       <AdventureOppurtunities />
       <Adventuremoments />
       <Whatwedo />
